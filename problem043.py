50337d152d1b59b94b873b41f505573219ad99c9
Max Marget <mmarget@physik.tu-berlin.de>
2017-06-02 16:11:42 +0200

    solved 43

--- problem043.py
+++ problem043.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python3
 #Project Euler Problem #043: Sub-string sivisibility
-bla = []
-for i in range(1234567890, 9876543211):
-    if sorted(list(str(i))) ==  ['0','1','2','3','4','5','6','7','8','9']:
-        bla.append(i)
-        print(i)
+from helpers import *
+bla = [1023456789]
+#for i in range(1234567890, 9876543211):
+#    if sorted(list(str(i))) ==  ['0','1','2','3','4','5','6','7','8','9']:
+#        bla.append(i)
+        #print(i)
+i = 1023456789
+while i != 9876543210:
+    ump = list(str(i))
+    lexicographicPermutation(ump)
+    i = int("".join(ump))
+    #i = int("".join(lexicographicPermutation(list(str(i)))))
+    bla.append(i)
 
-print("bla")
+
+#print(bla)
+blub = []
 for n in bla:
     if int(str(n)[1:4]) % 2 == 0 and int(str(n)[2:5]) % 3 == 0 and int(str(n)[3:6]) % 5 == 0 and int(str(n)[4:7]) % 7 == 0 and int(str(n)[5:8]) % 11 == 0 and int(str(n)[6:9]) % 13 == 0 and int(str(n)[7:10]) % 17 == 0:
+        blub.append(n)
         print(n)
+
+print(sum(blub))
